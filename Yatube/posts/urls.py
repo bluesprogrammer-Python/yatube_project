cad759adf558a9cb14da355e2c4ca42bb4cd45c7
Sergey Semenov <Raze.teatro2012@yandex.ru>
2022-05-16 15:39:07 +0300

    update

--- Yatube/posts/urls.py
+++ Yatube/posts/urls.py
@@ -7,5 +7,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<slug:slug>/', views.group_posts, name='group_posts'),
-] 
+    path('group/<slug>/', views.group_posts, name='group_list'),
+]
--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -4,20 +4,21 @@ from django.http import HttpResponse
 from .models import Post, Group
 from django.shortcuts import render, get_object_or_404
 
-def index(request):    
+
+def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
-       
+
+
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Yatube groups'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, 'posts/group_list.html', context) 
+    return render(request, template, context)
